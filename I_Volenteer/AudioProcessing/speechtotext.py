f59b3cff6ea1fbc939104c42c8bebaa534b03499
Shravan <shravanshenoy1998@hotmail.com>
2018-10-24 07:37:54 +0530

    Updated speech to text code

---
+++ I_Volenteer/AudioProcessing/speechtotext.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+YOUR_API_KEY = 'ENTER_YOUR_KEY_HERE'
+YOUR_AUDIO_FILE = './AudioTest2filtered'
+REGION = 'ENTER_YOUR_REGION' # westus, eastasia, northeurope 
+MODE = 'interactive'
+LANG = 'en-US'
+FORMAT = 'simple'
+
+
+def handler():
+    # 1. Get an Authorization Token
+    token = get_token()
+    # 2. Perform Speech Recognition
+    results = get_text(token, YOUR_AUDIO_FILE)
+    # 3. Print Results
+    print(results)
+
+def get_token():
+    # Return an Authorization Token by making a HTTP POST request to Cognitive Services with a valid API key.
+    url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+    headers = {
+        'Ocp-Apim-Subscription-Key': YOUR_API_KEY
+    }
+    r = requests.post(url, headers=headers)
+    token = r.content
+    return(token)
+
+def get_text(token, audio):
+    # Request that the Bing Speech API convert the audio to text
+    url = 'https://{0}.stt.speech.microsoft.com/speech/recognition/{1}/cognitiveservices/v1?language={2}&format={3}'.format(REGION, MODE, LANG, FORMAT)
+    headers = {
+        'Accept': 'application/json',
+        'Ocp-Apim-Subscription-Key': YOUR_API_KEY,
+        'Transfer-Encoding': 'chunked',
+        'Content-type': 'audio/wav; codec=audio/pcm; samplerate=16000',
+        'Authorization': 'Bearer {0}'.format(token)
+    }
+    r = requests.post(url, headers=headers, data=stream_audio_file(audio))
+    results = json.loads(r.content)
+    return results
+
+def stream_audio_file(speech_file, chunk_size=1024):
+    # Chunk audio file
+    with open(speech_file, 'rb') as f:
+        while 1:
+            data = f.read(1024)
+            if not data:
+                break
+            yield data
+
+if __name__ == '__main__':
+    handler()
