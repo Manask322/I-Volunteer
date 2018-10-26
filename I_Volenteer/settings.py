e8090da322bfc2e0df6440813438bfdd41967e36
Manask322 <16it226.manas@nitk.edu.in>
2018-10-26 22:20:06 +0530

    added

--- I_Volenteer/settings.py
+++ I_Volenteer/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-
+import dj_database_url
 # Application definition
 
 INSTALLED_APPS = [ 
@@ -75,10 +75,9 @@ WSGI_APPLICATION = 'I_Volenteer.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': dj_database_url.config(
+          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+      )
 }
 
 
