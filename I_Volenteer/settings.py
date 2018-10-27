cb9928ee4aad25cbea047f29a9a7abe8335937aa
Manask322 <16it226.manas@nitk.edu.in>
2018-10-27 10:23:33 +0530

    added files

--- I_Volenteer/settings.py
+++ I_Volenteer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +25,7 @@ SECRET_KEY = 'dk9*3dm_u@np)=k)9dtii+6%*q7@edk!u_x2m9^2pn($hw1*=-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','ivolunteerdisaster.herokuapp.com']   
+ALLOWED_HOSTS = ['*']   
 
 import dj_database_url
 # Application definition
@@ -117,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'NLP/static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'NLP/static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'NLP/static')
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/Audio_File/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'NLP/Audio_File')
+django_heroku.settings(locals())
